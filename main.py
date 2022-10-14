b6947d3804b893516622e87d45a6c63541a0aaca
Jingooooo <jingooo2@hexlant.com>
2022-10-14 18:14:47 +0900

    google login bug fix

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ address = "0x621a5AdA4f37074Eb10D06fe070FF9cac096B564"
 
 service = ChromeService(executable_path=ChromeDriverManager().install())
 options = ChromeOptions()
-options.add_argument("user-data-dir=/Users/user/Library/Application Support/Google/Chrome/Profile 2")
+# options.add_argument("headless")
+# options.add_argument("user-data-dir=/Users/user/Library/Application Support/Google/Chrome/Profile 2")
 driver = webdriver.Chrome(options=options, service=service)
 
 driver.get('https://goerlifaucet.com/')
@@ -35,15 +36,18 @@ if login_btn_text == 'Alchemy Login':
         time.sleep(5)
 
         if len(driver.window_handles) > 1:
-            driver.find_element(By.ID, "identifierId").send_keys(os.getenv("EMAIL") + Keys.ENTER)
+            driver.switch_to.window(driver.window_handles[1])
+            driver.find_element(By.NAME, "identifier").send_keys(os.getenv("EMAIL") + Keys.ENTER)
             time.sleep(3)
             driver.find_element(By.NAME, "password").send_keys(os.getenv("PASSWORD") + Keys.ENTER)
             driver.switch_to.window(driver.window_handles[0])
-            time.sleep(5)
+            time.sleep(10)
 
 
 address_input = driver.find_element(By.CLASS_NAME, "alchemy-faucet-panel-input-text").send_keys(address)
 time.sleep(3)
+
+driver.quit()
 # send_btn = driver.find_element(By.CLASS_NAME, "alchemy-faucet-button")
 # ActionChains(driver).move_to_element(address_input).send_keys(address).pause(3).perform()
 
